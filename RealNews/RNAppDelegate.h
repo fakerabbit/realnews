8af4d8360f79d7fbc6b58723b9787c898d93ecd3
Mirko <mirko@thedesignmob.com>
2013-09-18 16:38:14 -0400

    Initial Commit

---
+++ RealNews/RNAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RNAppDelegate.h
+//  RealNews
+//
+//  Created by Mirko Justiniano on 9/18/13.
+//  Copyright (c) 2013 Mirko Justiniano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RNAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RealNewsTests/RealNewsTests.m
@@ -0,0 +1,34 @@
+//
+//  RealNewsTests.m
+//  RealNewsTests
+//
+//  Created by Mirko Justiniano on 9/18/13.
+//  Copyright (c) 2013 Mirko Justiniano. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface RealNewsTests : XCTestCase
+
+@end
+
+@implementation RealNewsTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
